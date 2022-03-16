b24601d673505ebc06e1a23d93592b58456d3187
Felipe Ferreira Aguiar <felipeferreiraaguiar9@gmail.com>
2022-03-16 19:04:23 -0300

    Update handleSubmit.ts

--- src/scripts/handleSubmit.ts
+++ src/scripts/handleSubmit.ts
@@ -9,12 +9,11 @@ export const handleSubmit = async (e: FormEvent, userInfos: userInfos) => {
     console.log(userInfos);
     baseApi.post("/new-student", userInfos).then((res) => {
       if (res.status == 200) {
-        alert("Criado com sucesso");
-      } else if (res.status == 401) {
-        alert("Nome de usuário e/ou email já em uso");
-      } else {
-        alert("Verifique as informações inseridas");
-      }
+        return alert("Criado com sucesso");
+      } if (res.status == 401) {
+        return alert("Nome de usuário e/ou email já em uso");
+      } 
+        return alert("Verifique as informações inseridas");
     });
   } else {
     alert("Confirme a senha corretamente");
