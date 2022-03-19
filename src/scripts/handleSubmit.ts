ecef389034f35b18d34a5f11360ed89093b191d6
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-03-19 10:47:18 -0400

    Error handling

--- src/scripts/handleSubmit.ts
+++ src/scripts/handleSubmit.ts
@@ -8,12 +8,11 @@ export const handleSubmit = async (e: FormEvent, userInfos: userInfos) => {
   if (resenha.value == userInfos.password) {
     console.log(userInfos);
     baseApi.post("/new-student", userInfos).then((res) => {
+      if (res.status == 400) {
+        alert(res.data.error)
+      }
       if (res.status == 200) {
-        alert("Criado com sucesso");
-      } else if (res.status == 401) {
-        alert("Nome de usuário e/ou email já em uso");
-      } else {
-        alert("Verifique as informações inseridas");
+        alert(res.data.message)
       }
     });
   } else {
